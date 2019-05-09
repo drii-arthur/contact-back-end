4cdb5feea92a4ad522a26bba7bc7add035a9963f
drii-arthur <pandrir23@gmail.com>
2019-05-09 16:30:42 +0700

    running server on PORT 4000

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var http = require("http");
+const express = require('express');
+const app = express()
+const port = process.env.port || 4000;
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+
+app.listen(port, () => {
+  console.log(`Server Started on port : ${port}`)
+})
+
